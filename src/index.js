8d488e38c370d76dc4d01eadb8c3e68855a33b44
Sarju <sarju.hansaliya@gmail.com>
2018-11-18 01:09:42 +0530

    added index file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import GraphQLRequest from "./services";
+import AccountService from "./services/Account";
+import InvoiceHookupsService from "./services/InvoiceHookups";
+import InvoicesService from "./services/Invoices";
+import TimesheetEntriesService from "./services/TimesheetEntries";
+import TimesheetsService from "./services/Timesheets";
+
+export {
+  GraphQLRequest,
+  AccountService,
+  InvoiceHookupsService,
+  InvoicesService,
+  TimesheetEntriesService,
+  TimesheetsService
+};
